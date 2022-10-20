c501959fa0c9c2e6be9bf49ec0f4afe54bd1b4b2
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-10-20 09:19:37 +0530

    Initial Commit

---
+++ src/main/java/com/springboot/jwt/Spring_Boot_With_JWT_Json/SpringBootWithJwtJsonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.jwt.Spring_Boot_With_JWT_Json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJwtJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithJwtJsonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/jwt/Spring_Boot_With_JWT_Json/SpringBootWithJwtJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.jwt.Spring_Boot_With_JWT_Json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithJwtJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
